b5314fba919dc981885073091909621af52e8883
prgic <prgic@UNKNOWN-DEVICE>
2023-09-27 13:17:44 +0200

    Add project files.

---
+++ Digitron/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Digitron
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            buttonPlus = new Button();
+            buttonMinus = new Button();
+            panel1 = new Panel();
+            textBoxOutput = new TextBox();
+            buttonJednako = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(212, 212, 210);
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(4, 73);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 120);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += BrojDugme_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(212, 212, 210);
+            button2.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(130, 73);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 120);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += BrojDugme_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(212, 212, 210);
+            button3.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(256, 73);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 120);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += BrojDugme_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(212, 212, 210);
+            button4.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(4, 197);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 120);
+            button4.TabIndex = 6;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += BrojDugme_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(212, 212, 210);
+            button5.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button5.Location = new Point(130, 197);
+            button5.Name = "button5";
+            button5.Size = new Size(120, 120);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += BrojDugme_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(212, 212, 210);
+            button6.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button6.Location = new Point(256, 197);
+            button6.Name = "button6";
+            button6.Size = new Size(120, 120);
+            button6.TabIndex = 4;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += BrojDugme_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(212, 212, 210);
+            button7.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button7.Location = new Point(4, 323);
+            button7.Name = "button7";
+            button7.Size = new Size(120, 120);
+            button7.TabIndex = 9;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += BrojDugme_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(212, 212, 210);
+            button8.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(130, 323);
+            button8.Name = "button8";
+            button8.Size = new Size(120, 120);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += BrojDugme_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(212, 212, 210);
+            button9.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button9.Location = new Point(256, 323);
+            button9.Name = "button9";
+            button9.Size = new Size(120, 120);
+            button9.TabIndex = 7;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += BrojDugme_Click;
+            // 
+            // button0
+            // 
+            button0.BackColor = Color.FromArgb(212, 212, 210);
+            button0.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point);
+            button0.Location = new Point(4, 449);
+            button0.Name = "button0";
+            button0.Size = new Size(120, 120);
+            button0.TabIndex = 10;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = false;
+            button0.Click += BrojDugme_Click;
+            // 
+            // buttonPlus
+            // 
+            buttonPlus.BackColor = Color.FromArgb(255, 149, 0);
+            buttonPlus.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonPlus.Location = new Point(130, 449);
+            buttonPlus.Name = "buttonPlus";
+            buttonPlus.Size = new Size(120, 120);
+            buttonPlus.TabIndex = 11;
+            buttonPlus.Text = "+";
+            buttonPlus.UseVisualStyleBackColor = false;
+            buttonPlus.Click += PlusDugme_Click;
+            // 
+            // buttonMinus
+            // 
+            buttonMinus.BackColor = Color.FromArgb(255, 149, 0);
+            buttonMinus.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonMinus.Location = new Point(256, 449);
+            buttonMinus.Name = "buttonMinus";
+            buttonMinus.Size = new Size(120, 120);
+            buttonMinus.TabIndex = 12;
+            buttonMinus.Text = "-";
+            buttonMinus.UseVisualStyleBackColor = false;
+            buttonMinus.Click += MinusDugme_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(textBoxOutput);
+            panel1.Location = new Point(-3, -2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(512, 69);
+            panel1.TabIndex = 13;
+            // 
+            // textBoxOutput
+            // 
+            textBoxOutput.BorderStyle = BorderStyle.None;
+            textBoxOutput.Enabled = false;
+            textBoxOutput.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            textBoxOutput.Location = new Point(3, 14);
+            textBoxOutput.Name = "textBoxOutput";
+            textBoxOutput.Size = new Size(499, 36);
+            textBoxOutput.TabIndex = 0;
+            textBoxOutput.TextAlign = HorizontalAlignment.Right;
+            // 
+            // buttonJednako
+            // 
+            buttonJednako.BackColor = Color.DarkGray;
+            buttonJednako.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonJednako.Location = new Point(379, 449);
+            buttonJednako.Name = "buttonJednako";
+            buttonJednako.Size = new Size(120, 120);
+            buttonJednako.TabIndex = 14;
+            buttonJednako.Text = "=";
+            buttonJednako.UseVisualStyleBackColor = false;
+            buttonJednako.Click += JednakoDugme_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(255, 149, 0);
+            button10.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button10.Location = new Point(379, 73);
+            button10.Name = "button10";
+            button10.Size = new Size(120, 120);
+            button10.TabIndex = 15;
+            button10.Text = "CE";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += CEDugme_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(255, 149, 0);
+            button11.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button11.Location = new Point(379, 199);
+            button11.Name = "button11";
+            button11.Size = new Size(120, 120);
+            button11.TabIndex = 16;
+            button11.Text = "*";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += PomnoziDugme_Click;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(255, 149, 0);
+            button12.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point);
+            button12.Location = new Point(379, 325);
+            button12.Name = "button12";
+            button12.Size = new Size(120, 120);
+            button12.TabIndex = 17;
+            button12.Text = "/";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += PodeliDugme_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(28, 28, 28);
+            ClientSize = new Size(501, 582);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(buttonJednako);
+            Controls.Add(panel1);
+            Controls.Add(buttonMinus);
+            Controls.Add(buttonPlus);
+            Controls.Add(button0);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Digitron";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button buttonPlus;
+        private Button buttonMinus;
+        private Panel panel1;
+        private TextBox textBoxOutput;
+        private Button buttonJednako;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+    }
+}
---
+++ Digitron/Form1.cs
@@ -0,0 +1,103 @@
+using System.Text;
+using System.Net.Http;
+
+
+namespace Digitron
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double trenutnaVrednost = 0;
+        char trenutnaOperacija = '+';
+        double trenutniBroj = 0;
+        private void BrojDugme_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            textBoxOutput.Text += button.Text;
+        }
+        private void CEDugme_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text = "";
+        }
+        private void PlusDugme_Click(object sender, EventArgs e)
+        {
+            trenutniBroj = double.Parse(textBoxOutput.Text);
+            textBoxOutput.Text = "";
+            trenutnaOperacija = '+';
+        }
+
+        private void MinusDugme_Click(object sender, EventArgs e)
+        {
+            trenutniBroj = double.Parse(textBoxOutput.Text);
+            textBoxOutput.Text = "";
+            trenutnaOperacija = '-';
+        }
+
+        private void PomnoziDugme_Click(object sender, EventArgs e)
+        {
+            trenutniBroj = double.Parse(textBoxOutput.Text);
+            textBoxOutput.Text = "";
+            trenutnaOperacija = '*';
+        }
+
+        private void PodeliDugme_Click(object sender, EventArgs e)
+        {
+            trenutniBroj = double.Parse(textBoxOutput.Text);
+            textBoxOutput.Text = "";
+            trenutnaOperacija = '/';
+        }
+        bool PrikazanEasterEgg = false;
+        private async void JednakoDugme_Click(object sender, EventArgs e)
+        {
+            double noviBroj = double.Parse(textBoxOutput.Text);
+            trenutnaVrednost = trenutnaOperacija switch
+            {
+                '+' => trenutniBroj + noviBroj,
+                '-' => trenutniBroj - noviBroj,
+                '*' => trenutniBroj * noviBroj,
+                '/' => trenutniBroj / noviBroj,
+                _ => throw new ArgumentException("Invalidna operacija")
+            };
+
+            textBoxOutput.Text = trenutnaVrednost.ToString();
+            trenutnaVrednost = 0;
+            trenutnaOperacija = '+';
+
+            var timer = new System.Windows.Forms.Timer
+            {
+                Interval = 500,
+                Enabled = true
+            };
+
+            timer.Tick += (s, ev) =>
+            {
+                if (textBoxOutput.Text == "69" && !PrikazanEasterEgg)
+                {
+                    PrikazanEasterEgg = true;
+                    MessageBox.Show("Nice :)");
+                }
+
+                timer.Stop();
+                timer.Dispose();
+            };
+
+            await SendToDiscordWebhookAsync(textBoxOutput.Text);
+
+        }
+        private async Task SendToDiscordWebhookAsync(string result)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string webhookUrl = "https://discord.com/api/webhooks/1041151601341120522/OshLlpFySlNP7_QEV8BKKU-k33zCOtNW1LRFgBCGphO3aaV5S_ae8OS6L3_Xx9fKFZOz";
+                string message = result == "69" ? "Nice :)" : $"Rezultat je: {result}";
+
+                var content = new StringContent($"{{ \"content\": \"{message}\" }}", Encoding.UTF8, "application/json");
+                await client.PostAsync(webhookUrl, content);
+            }
+        }
+
+    }
+}
